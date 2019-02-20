61444bbb4eef4a1793267ec408c5e63de3c12c80
Christoph Krieger <christoph.krieger@iaas.uni-stuttgart.de>
2019-02-20 11:18:35 +0100

    Refactor unit test for watchdog pattern

--- constraintCheckingComponent/src/test/java/de/iaas/grossmann/cpe/engine/test/WatchdogTest.java
+++ constraintCheckingComponent/src/test/java/de/iaas/grossmann/cpe/engine/test/WatchdogTest.java
@@ -1,15 +1,15 @@
 package de.iaas.grossmann.cpe.engine.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Random;
 
 import de.iaas.grossmann.cpe.engine.handler.CEPEventHandler;
-import de.iaas.grossmann.cpe.engine.handler.EsperCEPEngine;
-import de.iaas.grossmann.cpe.engine.model.VirtualMachine;
+import de.iaas.grossmann.cpe.engine.handler.CEPStatementHandler;
+import de.iaas.grossmann.cpe.engine.handler.StatementViolationService;
+import de.iaas.grossmann.cpe.engine.subscriber.PatternStatementSubscriber;
+import de.iaas.grossmann.cpe.engine.test.generator.VirtualMachineEventGenerator;
+import de.iaas.grossmann.cpe.engine.test.generator.WorkloadEventGenerator;
+import de.iaas.grossmann.cpe.engine.test.util.PatternStatementUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,155 +17,143 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import de.iaas.grossmann.cpe.engine.handler.CEPStatementHandler;
-import de.iaas.grossmann.cpe.engine.handler.StatementViolationService;
-import de.iaas.grossmann.cpe.engine.subscriber.PatternStatementSubscriber;
-import de.iaas.grossmann.cpe.engine.test.generator.WorkloadEventGenerator;
-import de.iaas.grossmann.cpe.engine.test.util.PatternStatementUtils;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("test-application-context.xml")
 public class WatchdogTest {
 
-	@Autowired
-	private CEPStatementHandler statementHandler;
-	@Autowired
-	private StatementViolationService violationService;
-	@Autowired
-	private WorkloadEventGenerator generator;
-	@Autowired
-	private PatternStatementUtils patternUtils;
-	@Autowired
-	private CEPEventHandler eventHandler;
-	@Autowired
-	private EsperCEPEngine engine;
+    @Autowired
+    private CEPStatementHandler statementHandler;
+    @Autowired
+    private StatementViolationService violationService;
+    @Autowired
+    private PatternStatementUtils patternUtils;
+    @Autowired
+    private VirtualMachineEventGenerator vmEventGenerator;
 
-//	private static final VirtualMachine virtualMachineOneGroupOne = new VirtualMachine(1, 1);
-//	private static final VirtualMachine virtualMachineTwoGroupOne = new VirtualMachine(1, );
-	@Before
-	public void setup() {
-		statementHandler.deleteAllSubscribers();
-		violationService.deleteStatementsAndViolations();
-	}
-	@Test
-	public void testWatchdogNoViolationStaticNumber() throws FileNotFoundException, IOException {
-		PatternStatementSubscriber subscriber = patternUtils.preparePattern("Watchdog");
+    private static final String ACCOUNT_SERVICE_ID = "AccountService";
+    private static final String INVENTORY_SERVICE_ID = "InventoryService";
+    private static final String SHIPPING_SERVICE_ID = "ShippingService";
+    private static final String PATTERN_NAME = "Watchdog";
 
-		// generate three VMs in a scaling group
-		for (int i = 0; i < 2; i++) {
-			eventHandler.handle(new VirtualMachine(Integer.toString(i), "AccountService"));
-		}
+    private static PatternStatementSubscriber subscriber;
 
-		assertEquals(0, this.violationService.getViolationsFor(subscriber));
-	}
+    @Before
+    public void setup() throws IOException {
+        statementHandler.deleteAllSubscribers();
+        violationService.deleteStatementsAndViolations();
+        this.subscriber = patternUtils.preparePattern(PATTERN_NAME);
+    }
 
-	@Test
-	public void testWatchdogViolationDecreasingNumber() throws Exception {
-		PatternStatementSubscriber subscriber = patternUtils.preparePattern("Watchdog");
+    @Test
+    public void testWatchdogNoViolationStaticNumber() throws FileNotFoundException, IOException, InterruptedException {
+        // generate events of three VMs in the Account Service scaling group
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 3);
+        }
+        // generate events of two VMs in the Inventory Service scaling group
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(INVENTORY_SERVICE_ID, 2);
+        }
+        assertEquals(0, this.violationService.getViolationsFor(subscriber));
+    }
 
-		for (int i = 0; i < 5; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(3);
-		}
-		for (int i = 0; i < 10; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(2);
-		}
+    @Test
+    public void testWatchdogViolationDecreasingNumber() throws Exception {
 
-		assertTrue(this.violationService.getViolationsFor(subscriber) >= 1);
-	}
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 3);
+        }
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 2);
+        }
 
-	@Test
-	public void shouldDifferntiateBetweenScalingGroups() throws Exception {
-		PatternStatementSubscriber subscriber = patternUtils.preparePattern("Watchdog");
+        assertTrue(this.violationService.getViolationsFor(subscriber) >= 1); // warum ist die violation nicht immer 1?
+    }
 
-			for (int i = 0; i < 3; i++) {
-				eventHandler.handle(new VirtualMachine(Integer.toString(i), "AccountService"));
-			}
+    @Test
+    public void shouldDifferntiateBetweenScalingGroups() throws Exception {
 
-			Thread.sleep(500);
-			for (int i = 0; i < 2; i++) {
-				eventHandler.handle(new VirtualMachine(Integer.toString(i), "InventoryService"));
-			}
-			assertEquals(0, this.violationService.getViolationsFor(subscriber));
-	}
+        for (int i = 0; i < 3; i++) {
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 3);
+        }
 
-	@Test
-	public void testWatchdogNoViolationDecreasingThenIncreasingNumber() throws Exception {
-		PatternStatementSubscriber subscriber = patternUtils.preparePattern("Watchdog");
+        Thread.sleep(500);
+        for (int i = 0; i < 3; i++) {
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(INVENTORY_SERVICE_ID, 2);
+        }
+        assertEquals(0, this.violationService.getViolationsFor(subscriber));
+    }
 
-		for (int i = 0; i < 5; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(3);
-		}
-		for (int i = 0; i < 3; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(2);
-		}
-		for (int i = 0; i < 5; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(3);
-		}
+    @Test
+    public void testWatchdogNoViolationDecreasingThenIncreasingNumber() throws Exception {
 
-		assertEquals(0, this.violationService.getViolationsFor(subscriber));
-	}
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 3);
+        }
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 2);
+        }
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 3);
+        }
 
-	@Test
-	public void testWatchdogViolationDoubleDecreasingNumber() throws Exception {
-		PatternStatementSubscriber subscriber = patternUtils.preparePattern("Watchdog");
+        assertEquals(0, this.violationService.getViolationsFor(subscriber));
+    }
 
-		for (int i = 0; i < 5; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(3);
-		}
-		for (int i = 0; i < 3; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(2);
-		}
-		for (int i = 0; i < 3; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(1);
-		}
-		for (int i = 0; i < 10; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(2);
-		}
+    @Test
+    public void testWatchdogViolationDoubleDecreasingNumber() throws Exception {
 
-		assertTrue(this.violationService.getViolationsFor(subscriber) >= 1);
-	}
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 3);
+        }
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 2);
+        }
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 1);
+        }
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 2);
+        }
 
-	@Test
-	public void testWatchdogNoViolationDoubleDecreasingDoubleIncreasingNumber() throws Exception {
-		PatternStatementSubscriber subscriber = patternUtils.preparePattern("Watchdog");
+        assertTrue(this.violationService.getViolationsFor(subscriber) >= 1);
+    }
 
-		for (int i = 0; i < 5; i++) {
-			Thread.sleep(100);
-			generator.generateRandomCpuWorkload(3);
-		}
+    @Test
+    public void testWatchdogNoViolationDoubleDecreasingDoubleIncreasingNumber() throws Exception {
 
-		Thread.sleep(100);
-		generator.generateRandomCpuWorkload(1);
-		Thread.sleep(100);
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(100);
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 3);
+        }
 
-		Thread.sleep(50);
-		generator.generateRandomCpuWorkload(2);
-		Thread.sleep(10);
+        Thread.sleep(100);
+        vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 1);
+        Thread.sleep(100);
 
-		for (int i = 0; i < 5; i++) {
-			generator.generateRandomCpuWorkload(3);
-			Thread.sleep(100);
-		}
+        Thread.sleep(50);
+        vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 2);
+        Thread.sleep(10);
 
-		assertTrue(this.violationService.getViolationsFor(subscriber) == 0);
-	}
+        for (int i = 0; i < 5; i++) {
+            vmEventGenerator.generateVirtualMachineEventsOfScalingGroup(ACCOUNT_SERVICE_ID, 3);
+            Thread.sleep(100);
+        }
 
-//	@Test
-//	public void testStaticWorkloadNotValidWithBigVariations() throws Exception {
-//		PatternStatementSubscriber subscriber = patternUtils.preparePattern("StaticWorkload");
-//
-//		generator.generateStaticCpuWorkload(TEST_DURATION, TEST_EVENT_BREAKTIME, 0.3, 0.2);
-//
-//		assertTrue(this.violationService.getViolationsFor(subscriber) >= 1);
-//	}
-
+        assertTrue(this.violationService.getViolationsFor(subscriber) == 0);
+    }
 }
