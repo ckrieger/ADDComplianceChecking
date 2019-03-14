6aacc005328efc06e6b384a7023503098fecf24e
Christoph Krieger <christoph.krieger@iaas.uni-stuttgart.de>
2019-03-14 13:53:45 +0100

    add dockerfiles

--- motivation-scenario/inventory-service/server.js
+++ motivation-scenario/inventory-service/server.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var os = require("os");
 
+const hostname = os.hostname();
 const port = 5000;
 // parse application/json
 app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
-  res.send('hello from inventory-service')
+  res.send('hello from inventory-service with hostname: ' + hostname)
 })
 
 app.listen(port, function() {
