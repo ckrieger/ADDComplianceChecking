c757759018b65e15b52a526e263d9db80b92c872
Christoph Krieger <christoph.krieger@iaas.uni-stuttgart.de>
2019-03-22 10:54:48 +0100

    add ui for inventory-service

--- motivation-scenario/inventory-service/server.js
+++ motivation-scenario/inventory-service/server.js
@@ -3,18 +3,48 @@ var bodyParser = require('body-parser')
 var app = express()
 var os = require("os");
 var sender = require('./message-service')
+const request = require('request-promise-native')
 
 const hostname = os.hostname();
 const port = 5000;
+
+let failureCount = 0;
+
+//const shippinServiceUri = 'http://motivation-scenario_shipping-service:8088';
+const shippinServiceUri = 'http://localhost:8088';
+
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-  res.send('hello from inventory-service with hostname: ' + hostname)
+app.post('/sendRequest', function(req, res) {
+  sendRequestToShippingService(res);
 })
 
+function sendRequestToShippingService(response){
+  var options = {
+      method: 'GET',
+      uri: shippinServiceUri,
+      json: true 
+  };
+   
+  request(options)
+      .then(function (parsedBody) {
+          console.log('Resopnse of Shipping-Service: ' + parsedBody);
+          failureCount = 0;
+          response.json({response: "Request Success", failureCount: failureCount })
+      })
+      .catch(function (err) {
+        failureCount ++;
+          response.json({response: "Request Failed", failureCount: failureCount })
+          console.log(err);
+      });
+}
+
 // start sending messages to queue
-setInterval(sender.sendMessage, 1500);
+//setInterval(sender.sendMessage, 1500);
+
+//serve static file (index.html, images, css)
+app.use(express.static(__dirname + '/views'));
 
 app.listen(port, function() {
   console.log('Inventory Service is listening on port ' + port)
