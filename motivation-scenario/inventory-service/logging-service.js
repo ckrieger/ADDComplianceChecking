fa337a3c286b392909775a257aa2a2ff03cd4954
Christoph Krieger <christoph.krieger@iaas.uni-stuttgart.de>
2019-05-22 15:06:27 +0200

    change default queue name

--- motivation-scenario/inventory-service/logging-service.js
+++ motivation-scenario/inventory-service/logging-service.js
@@ -1,5 +1,6 @@
 var amqp = require("amqplib/callback_api");
 var os = require("os");
+var osUtils = require("os-utils");
 const log = require('simple-node-logger').createSimpleFileLogger(`loggingvolume/${os.hostname()}.log`);
 
 var exports = module.exports = {};
@@ -13,12 +14,14 @@ function getScalingGroupId(){
 }
 
 exports.logContainerInfo = function logContainerInfo() {
-  var data = {
-    vmId: getVMId(),
-    scalingGroupId: getScalingGroupId(),
-    cpu: "0.5"
-  };
-  log.info(data);
+  osUtils.cpuUsage(function(value){
+    var data = {
+      vmId: getVMId(),
+      scalingGroupId: getScalingGroupId(),
+      cpu: value
+    };
+    log.info(data);
+  });
 }
 
 exports.logHttpResponseStatus = function logHttpResponseStatus(statusCode) {
